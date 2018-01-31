5677371169358e9c349b000cf9884f369c1f92a5
Seeraj <sinaisix@gmail.com>
2018-01-31 08:32:13 +0000

    Added server request and response filters

--- src/main/java/com/pedantic/config/ServerRequestFilter.java
+++ src/main/java/com/pedantic/config/ServerRequestFilter.java
@@ -1,4 +1,22 @@
 package com.pedantic.config;
 
-public class ServerRequestFilter {
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+@PreMatching
+public class ServerRequestFilter implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        //X-HTTP-METHOD - POST
+
+        String methodOverride = requestContext.getHeaderString("X-Http-Method-Override");
+        if (methodOverride != null && !methodOverride.isEmpty()) {
+            requestContext.setMethod(methodOverride);
+
+        }
+    }
 }
--- src/main/java/com/pedantic/config/ServerResponseFilter.java
+++ src/main/java/com/pedantic/config/ServerResponseFilter.java
@@ -1,4 +1,25 @@
 package com.pedantic.config;
 
-public class ServerResponseFilter {
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class ServerResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+//check if method is get
+        //tell client to cache using cache control abstraction for 150 secs
+        if (requestContext.getMethod().equals("GET")) {
+            CacheControl cacheControl = new CacheControl();
+            cacheControl.setMaxAge(150);
+            responseContext.getHeaders().add("Cache-Control", cacheControl);
+
+        }
+
+    }
 }
